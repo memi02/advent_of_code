bcaab692cdedc9a9e71047eaef3cdccdd625ea33
memi02 <emilio.barausse@gmail.com>
2022-12-06 12:31:55 +0100

    day05 complete

---
+++ 2022/day05/1.cpp
@@ -0,0 +1,97 @@
+#include <fstream>
+#include <iostream>
+#include <stack>
+#include <string>
+
+void printStack(std::stack<char> &s);
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << "<input_file>" << std::endl;
+        exit(0);
+    }
+    std::fstream in;
+    in.open(argv[1], std::ios::in);
+    if (in.fail())
+    {
+        std::cout << "Error opening input file" << std::endl;
+        exit(0);
+    }
+
+    std::fstream in_stacks;
+    in_stacks.open("input_stacks.txt", std::ios::in);
+
+    std::string buffer;
+    std::stack<char> stacks[9];
+
+    for (int i = 0; getline(in_stacks, buffer); i++)
+    {
+        while (!buffer.empty())
+        {
+            stacks[i].push(buffer.back());
+            buffer.pop_back();
+        }
+    }
+    /*     for (int i = 0; i < 9; i++)
+        {
+            std::cout << "stack " << i << " ";
+            printStack(stacks[i]);
+        } */
+
+    while (in >> buffer)
+    {
+        int nPieces, from, to;
+        in >> buffer;
+        nPieces = std::stoi(buffer);
+        in >> buffer;
+        in >> buffer;
+        from = std::stoi(buffer);
+        in >> buffer;
+        in >> buffer;
+        to = std::stoi(buffer);
+
+        for (int i = 0; i < nPieces; i++)
+        {
+            stacks[to - 1].push(stacks[from - 1].top());
+            stacks[from - 1].pop();
+        }
+
+        /*         std::cout << std::endl
+                          << "move " << nPieces << " from " << from << " to " << to;
+
+                std::cout << std::endl;
+                for (int i = 0; i < 9; i++)
+                {
+                    std::cout << "stack " << i << " ";
+                    printStack(stacks[i]);
+                } */
+    }
+
+    for (int i = 0; i < 9; i++)
+    {
+        std::cout << stacks[i].top();
+    }
+    std::cout << std::endl;
+
+    in.close();
+    return 0;
+}
+
+void printStack(std::stack<char> &s)
+{
+    std::stack<char> tmp;
+    while (!s.empty())
+    {
+        tmp.push(s.top());
+        s.pop();
+    }
+    while (!tmp.empty())
+    {
+        std::cout << tmp.top() << " ";
+        s.push(tmp.top());
+        tmp.pop();
+    }
+    std::cout << std::endl;
+}
