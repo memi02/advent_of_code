24c852710f6bb884ef9470136a00536cdcbe27e1
Emilio Barausse <emilio@MacBook-Pro-di-Emilio.local>
2024-12-11 17:27:42 +0100

    day05 part2

--- 2024/day05/src/main.rs
+++ 2024/day05/src/main.rs
@@ -10,7 +10,7 @@ fn main() -> std::io::Result<()> {
     let mut rules_str = String::new();
     buf_reader.read_to_string(&mut rules_str)?;
 
-    let mut prints = rules_str.split_off(rules_str.find("\n\n").unwrap());
+    let prints = rules_str.split_off(rules_str.find("\n\n").unwrap());
 
     // A map of rules, where the key is a number and the value is a vector of the ones that should be before it
     let mut rules = HashMap::with_capacity(90);
@@ -34,33 +34,53 @@ fn main() -> std::io::Result<()> {
     let mut result = 0;
 
     for line in prints.lines().skip(2) {
-        let line = line.split(',').collect::<Vec<&str>>();
-        if line
-            .iter()
-            .enumerate()
-            .map(|(i, e)| match rules.get(e) {
-                // set value to true if it respects all rules, false otherwise
-                None => true,
-                Some(v) => {
-                    let mut res = true;
-                    for val in line.get(i..).expect("i should never be out of bound") {
-                        if v.binary_search(val).is_ok() {
-                            res = false;
-                        }
+        let mut line = line.split(',').collect::<Vec<&str>>();
+        let mut found_errors = false;
+        for mut i in (0..line.len()).rev() {
+            match find_error(&rules, i, &line) {
+                None => {}
+                Some(errors) => {
+                    found_errors = true;
+                    for e in errors {
+                        let e_value = line.remove(e);
+                        line.insert(i, e_value);
+                        i += 1;
                     }
-                    res
                 }
-            })
-            .find(|e| !e)
-            .is_none()
-        {
+            }
+        }
+        if found_errors {
             result += line[line.len() / 2]
                 .parse::<i32>()
-                .expect("All values should be numbers");
-        };
+                .expect("Should be a number");
+        }
     }
 
     println!("The answer is: {result}");
 
     Ok(())
+}
+
+// returns None if every rule associated with current is valid, returns a vector of error indexes otherwise
+fn find_error(
+    rules: &HashMap<&str, Vec<&str>>,
+    current_pos: usize,
+    line: &[&str],
+) -> Option<Vec<usize>> {
+    match rules.get(line[current_pos]) {
+        None => None,
+        Some(v) => {
+            let mut errors = vec![];
+            for i in current_pos + 1..line.len() {
+                if v.binary_search(&line[i]).is_ok() {
+                    errors.push(i);
+                }
+            }
+            if errors.is_empty() {
+                None
+            } else {
+                Some(errors)
+            }
+        }
+    }
 }
