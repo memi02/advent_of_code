bb24c5bbc0cccce4dffa539a685f538a7f817d5a
Emilio Barausse <emilio@MacBook-Pro-di-Emilio.local>
2024-12-16 22:48:05 +0100

    day06 part2

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -12,56 +12,52 @@ fn main() -> std::io::Result<()> {
 
     let input_table_len = input.lines().count();
 
-    let mut map = Array2D::from_iter_row_major(
+    let map = Array2D::from_iter_row_major(
         input.lines().map(|l| l.chars()).flatten(),
         input_table_len,
         input_table_len,
     )
     .expect("The input should contain enough charactes to fill the table");
 
-    let mut guard_pos = (0, 0);
-    for i in 0..map.row_len() {
-        for j in 0..map.column_len() {
-            if map[(i, j)] == '^' {
-                guard_pos = (i, j);
-                break;
+    let guard_pos = find_guard_start_pos(&map);
+    let guard_dir = Direction::Up;
+
+    let mut original_walk_map = map.clone();
+    walk_guard(&mut original_walk_map, guard_pos, guard_dir);
+
+    let mut result = 0;
+
+    for block_pos in original_walk_map.indices_row_major() {
+        // can't place a block in the guard original position
+        if block_pos == guard_pos {
+            continue;
+        }
+        match original_walk_map[block_pos] {
+            // Adding a block can make a difference only if I'm on the original path of the guard
+            '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' => {
+                let mut local_map = map.clone();
+                local_map
+                    .set(block_pos.0, block_pos.1, 'O')
+                    .expect("This should always be inbound");
+
+                if walk_guard(&mut local_map, guard_pos, guard_dir) {
+                    result += 1;
+                    // for row in local_map.rows_iter() {
+                    //     println!("{}", row.collect::<String>());
+                    // }
+                    // println!();
+                }
             }
-        }
-    }
-    let mut guard_dir = Direction::Up;
-
-    'move_guard: loop {
-        println!("Guard at: {guard_pos:?} facing {guard_dir:?}");
-        map.set(guard_pos.0, guard_pos.1, 'X')
-            .expect("should never be out of bound");
-        let mut is_dir_valid = false;
-        while !is_dir_valid {
-            match move_in_direction(&guard_pos, &guard_dir, map.row_len()) {
-                // If i'm evaluating going out of bound I can and so I will, so I'm done
-                Err(_) => break 'move_guard,
-                Ok(next_pos) => match map[next_pos] {
-                    '#' => guard_dir.next(),
-                    _ => is_dir_valid = true,
-                },
-            };
-        }
-        guard_pos = move_in_direction(&guard_pos, &guard_dir, map.row_len())
-            .expect("This should always be valid");
-    }
-
-    for row in map.rows_iter() {
-        println!("{}", row.collect::<String>());
+            _ => continue,
+        };
     }
 
     // When I exit the loop the guard will be out of bound and I have to count the X's
-    println!(
-        "The answer is: {}",
-        map.elements_row_major_iter().filter(|c| c == &&'X').count()
-    );
+    println!("The answer is: {}", result);
     Ok(())
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Direction {
     Up,
     Down,
@@ -116,4 +112,71 @@ fn move_in_direction(
             }
         }
     }
+}
+
+fn find_guard_start_pos(map: &Array2D<char>) -> (usize, usize) {
+    let mut res = (0, 0);
+    for i in 0..map.row_len() {
+        for j in 0..map.column_len() {
+            if map[(i, j)] == '^' {
+                res = (i, j);
+                break;
+            }
+        }
+    }
+    res
+}
+
+/// returns true if the guard gets stuck in a loop, false otherwise
+/// marks the guard path with X on the map
+fn walk_guard(
+    map: &mut Array2D<char>,
+    mut guard_pos: (usize, usize),
+    mut guard_dir: Direction,
+) -> bool {
+    'move_guard: loop {
+        let mut is_dir_valid = false;
+        while !is_dir_valid {
+            match move_in_direction(&guard_pos, &guard_dir, map.row_len()) {
+                // If i'm evaluating going out of bound I can and so I will, so I'm done
+                Err(_) => break 'move_guard,
+                Ok(next_pos) => match map[next_pos] {
+                    '#' | 'O' => guard_dir.next(),
+                    _ => is_dir_valid = true,
+                },
+            };
+        }
+        map.set(
+            guard_pos.0,
+            guard_pos.1,
+            match map[guard_pos] {
+                '1' => '2',
+                '2' => '3',
+                '3' => '4',
+                '4' => '5',
+                '5' => '6',
+                '6' => '7',
+                '7' => '8',
+                '8' => '9',
+                '9' => '0',
+                '0' => '0',
+                _ => '1',
+            },
+        )
+        .expect("should never be out of bound");
+        if map[guard_pos] == '0'
+            && map[move_in_direction(&guard_pos, &guard_dir, map.row_len())
+                .expect("This should always be valid")]
+                == '0'
+        {
+            // If I have already been twice here and In the cell I will go, I should be in a loop
+            return true;
+        }
+        guard_pos = move_in_direction(&guard_pos, &guard_dir, map.row_len())
+            .expect("This should always be valid");
+    }
+    // if I'm here I am about to leave the map, so I should mark the guard position with an X
+    map.set(guard_pos.0, guard_pos.1, '1')
+        .expect("should never be out of bound");
+    false
 }
