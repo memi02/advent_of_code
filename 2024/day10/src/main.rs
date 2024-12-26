e74599bb6e703842ad6806caf20865999a492798
Memi02 <emilio.barausse@gmail.com>
2024-12-26 21:27:32 +0100

    day10 part2
    deleting the visited_map means that each recursion will get at most one
    9 and in each call I have to find a new path or an invalid path

--- 2024/day10/src/main.rs
+++ 2024/day10/src/main.rs
@@ -33,10 +33,7 @@ fn main() -> std::io::Result<()> {
 
     let result = trailheads
         .iter()
-        .map(|head| {
-            let mut visited_map = Array2D::filled_with(false, map_size, map_size);
-            follow_trail(&map, &mut visited_map, *head)
-        })
+        .map(|head| follow_trail(&map, *head))
         .inspect(|tot| println!("{}", tot))
         .sum::<u32>();
 
@@ -44,35 +41,33 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn follow_trail(map: &Array2D<u32>, visited_map: &mut Array2D<bool>, pos: (usize, usize)) -> u32 {
-    visited_map[pos] = true;
-
+fn follow_trail(map: &Array2D<u32>, pos: (usize, usize)) -> u32 {
     let mut res = 0;
     if map[pos] == 9 {
         res += 1;
     }
 
     let pos_to_check = (pos.0 + 1, pos.1);
-    if check_cell(map, visited_map, map[pos], pos_to_check) {
-        res += follow_trail(map, visited_map, pos_to_check);
+    if check_cell(map, map[pos], pos_to_check) {
+        res += follow_trail(map, pos_to_check);
     }
 
     let pos_to_check = (pos.0, pos.1 + 1);
-    if check_cell(map, visited_map, map[pos], pos_to_check) {
-        res += follow_trail(map, visited_map, pos_to_check);
+    if check_cell(map, map[pos], pos_to_check) {
+        res += follow_trail(map, pos_to_check);
     }
 
     if pos.0 != 0 {
         let pos_to_check = (pos.0 - 1, pos.1);
-        if check_cell(map, visited_map, map[pos], pos_to_check) {
-            res += follow_trail(map, visited_map, pos_to_check);
+        if check_cell(map, map[pos], pos_to_check) {
+            res += follow_trail(map, pos_to_check);
         }
     }
 
     if pos.1 != 0 {
         let pos_to_check = (pos.0, pos.1 - 1);
-        if check_cell(map, visited_map, map[pos], pos_to_check) {
-            res += follow_trail(map, visited_map, pos_to_check);
+        if check_cell(map, map[pos], pos_to_check) {
+            res += follow_trail(map, pos_to_check);
         }
     }
 
@@ -80,12 +75,6 @@ fn follow_trail(map: &Array2D<u32>, visited_map: &mut Array2D<bool>, pos: (usize
 }
 
 // will check without painicing if pos is +-1 from value
-fn check_cell(
-    map: &Array2D<u32>,
-    visited_map: &Array2D<bool>,
-    value: u32,
-    pos: (usize, usize),
-) -> bool {
+fn check_cell(map: &Array2D<u32>, value: u32, pos: (usize, usize)) -> bool {
     map.get(pos.0, pos.1).is_some_and(|v| *v == value + 1)
-        && visited_map.get(pos.0, pos.1).is_some_and(|b| !b)
 }
